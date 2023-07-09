051c0e4c3087f31d10eb013202e97bc5f1825b0c
Petro Abramov <sebshe3@gmail.com>
2023-07-09 15:08:30 +0300

    Added views for cinema fpp

--- cinema_service/settings.py
+++ cinema_service/settings.py
@@ -37,10 +37,16 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "rest_framework",
+    "drf_spectacular",
+    "debug_toolbar",
+    "cinema",
+    "user",
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
